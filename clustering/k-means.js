0ec547cdf6d1c056d9c6943e0b2a4ecd9a97b411
unknown <karpuk-05@mail.ru>
2024-03-25 01:01:58 +0700

    base alg

--- clustering/k-means.js
+++ clustering/k-means.js
@@ -18,21 +18,19 @@ function cluster(clusterCount, points, canvas, ctx) {
     
     const uniqueColors = generateUniqueColors(clusterCount);
 
-    // Инициализируем кластеры случайным образом
     for (let i = 0; i < clusterCount; i++) {
         clusters.push({
             x: Math.random() * canvas.width,
             y: Math.random() * canvas.height,
             points: [],
-            color: uniqueColors[i] // Выбираем уникальный цвет для каждого кластера
+            color: uniqueColors[i] 
         });
     }
-    // Функция для вычисления расстояния между двумя точками
+    
     function distance(point1, point2) {
         return Math.sqrt(Math.pow(point1.x - point2.x, 2) + Math.pow(point1.y - point2.y, 2));
     }
     
-    // Присваиваем каждую точку к ближайшему кластеру
     points.forEach(point => {
         let minDistance = Number.MAX_VALUE;
         let closestCluster = null;
@@ -48,7 +46,6 @@ function cluster(clusterCount, points, canvas, ctx) {
         closestCluster.points.push(point);
     });
     
-    // Перемещаем центры кластеров в среднее геометрическое точек в каждом кластере
     clusters.forEach(cluster => {
         if (cluster.points.length > 0) {
             let sumX = cluster.points.reduce((acc, p) => acc + p.x, 0);
@@ -58,7 +55,6 @@ function cluster(clusterCount, points, canvas, ctx) {
         }
     });
     
-    // Очищаем предыдущие кластеры и перерисовываем их
     ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
     clusters.forEach(cluster => {
         ctx.fillStyle = cluster.color;
