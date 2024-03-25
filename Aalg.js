1cd57daff14805008f003dd039fd155173fe157e
💗 💗💗 <sasha_goroschenko@mail.ru>
2024-03-25 12:46:19 +0700

    new:fix start finish

--- Aalg.js
+++ Aalg.js
@@ -2,8 +2,8 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 var num, cellSize;
 var cells;
-var currentStart=[];
-var currentFinish=[];
+var currentStart;
+var currentFinish;
 
 function input()
 {
@@ -51,6 +51,8 @@ function generateLabyrinth()
   var y = Math.floor(Math.random() * num / 2) * 2;
   cells[y][x]="empty";
   isUsed[y][x]=true;
+  currentStart={x:x,y:y,type:cells[y][x]};//установка старта
+
   //добавление возможных точек перехода
   var check = [];
   if (y - 2 >= 0) {
@@ -76,6 +78,7 @@ function generateLabyrinth()
     y = check[index].y;
     cells[y][x]="empty";
     check.splice(index,1);
+    currentFinish={x:x,y:y,type:cells[y][x]};//установка финиша
 
     var d = [0,1,2,3];
     while (d.length>0) {
@@ -116,8 +119,9 @@ function generateLabyrinth()
       isUsed[y][x+2]=true;
     }
   }
-  //отрисовка
-  cells[num-2][num-1]="empty";//проход до финиша
+}
+function drawLabyrinth()
+{
   for (let y = 0; y < num; y++) {
     for (let x = 0; x < num; x++) {
       if (cells[y][x]=="empty")
@@ -130,16 +134,25 @@ function generateLabyrinth()
 }
 function setDefaultStartFinish()
 {
-  currentStart.length=0;
-  currentFinish.length=0;
+  cells[num-2][num-1]="empty";//проход до финиша
+  cells[0][1]="empty";//проход до старта
   ctx.fillStyle = 'green';
   ctx.fillRect(1, 1, cellSize-2, cellSize-2);
-  currentStart.push({x:0,y:0,type:cells[0][0]});
+  currentStart={x:0,y:0,type:cells[0][0]};
   cells[0][0]="start";
   ctx.fillStyle = 'red';
   ctx.fillRect((num-1)*cellSize+1, (num-1)*cellSize+1, cellSize-2, cellSize-2);
-  currentFinish.push({x:num-1,y:num-1,type:cells[num-1][num-1]});
+  currentFinish={x:num-1,y:num-1,type:cells[num-1][num-1]};
   cells[num-1][num-1]="finish";
+}
+function setStartFinish()
+{
+  ctx.fillStyle = 'green';
+  ctx.fillRect(currentStart.x*cellSize+1, currentStart.y*cellSize+1, cellSize-2, cellSize-2);
+  cells[currentStart.y][currentStart.x]="start";
+  ctx.fillStyle = 'red';
+  ctx.fillRect(currentFinish.x*cellSize+1, currentFinish.y*cellSize+1, cellSize-2, cellSize-2);
+  cells[currentFinish.y][currentFinish.x]="finish";
 }
 function create()
 {
@@ -147,6 +160,8 @@ function create()
   drawGrid();
   generateLabyrinth();
   setDefaultStartFinish();
+  drawLabyrinth();
+  //setStartFinish();
 }
 
 var change = document.getElementById('redact').value;
@@ -162,11 +177,11 @@ canvas.addEventListener('click', function(event) {
   if (change=="walls")
   {
     if (cells[Math.floor(y/cellSize)][Math.floor(x/cellSize)]=="wall"){
-      if (Math.floor(y/cellSize)==currentStart[0].y && Math.floor(x/cellSize)==currentStart[0].x && startFinish!="finish"){
+      if (Math.floor(y/cellSize)==currentStart.y && Math.floor(x/cellSize)==currentStart.x && startFinish!="finish"){
         ctx.fillStyle = 'green';
         cells[Math.floor(y/cellSize)][Math.floor(x/cellSize)]="start";
       }
-      else if (Math.floor(y/cellSize)==currentFinish[0].y && Math.floor(x/cellSize)==currentFinish[0].x  && startFinish!="start"){
+      else if (Math.floor(y/cellSize)==currentFinish.y && Math.floor(x/cellSize)==currentFinish.x  && startFinish!="start"){
         ctx.fillStyle = 'red';
         cells[Math.floor(y/cellSize)][Math.floor(x/cellSize)]="finish";
       }
@@ -183,11 +198,11 @@ canvas.addEventListener('click', function(event) {
   }
   if (change=="begin")
   {
-    if (cells[currentStart[0].y][currentStart[0].x]=="start"){
+    if (cells[currentStart.y][currentStart.x]=="start"){
     //убрать старый старт
-    ctx.fillStyle = currentStart[0].type=="wall"?'black':currentStart[0].type=="empty"?'white':currentStart[0].type=="start"?'green':'red';
-    ctx.fillRect(currentStart[0].x*cellSize+1, currentStart[0].y*cellSize+1, cellSize-2, cellSize-2); 
-    cells[currentStart[0].y][currentStart[0].x]=currentStart[0].type;
+    ctx.fillStyle = currentStart.type=="wall"?'black':currentStart.type=="empty"?'white':currentStart.type=="start"?'green':'red';
+    ctx.fillRect(currentStart.x*cellSize+1, currentStart.y*cellSize+1, cellSize-2, cellSize-2); 
+    cells[currentStart.y][currentStart.x]=currentStart.type;
     }
     if (cells[Math.floor(y/cellSize)][Math.floor(x/cellSize)]=="finish"){//для отмены стен
       startFinish="start";
@@ -195,22 +210,22 @@ canvas.addEventListener('click', function(event) {
     //обозначить новый старт
     ctx.fillStyle = 'green';
     ctx.fillRect(Math.floor(x/cellSize)*cellSize+1, Math.floor(y/cellSize)*cellSize+1, cellSize-2, cellSize-2);
-    currentStart[0]={x:Math.floor(x/cellSize),y:Math.floor(y/cellSize),type:cells[Math.floor(y/cellSize)][Math.floor(x/cellSize)]};
+    currentStart={x:Math.floor(x/cellSize),y:Math.floor(y/cellSize),type:cells[Math.floor(y/cellSize)][Math.floor(x/cellSize)]};
     cells[Math.floor(y/cellSize)][Math.floor(x/cellSize)]="start";
   }
   if (change=="end")
   {
-    if (cells[currentFinish[0].y][currentFinish[0].x]=="finish"){
-    ctx.fillStyle = currentFinish[0].type=="wall"?'black':currentFinish[0].type=="empty"?'white':currentFinish[0].type=="start"?'green':'red';
-    ctx.fillRect(currentFinish[0].x*cellSize+1, currentFinish[0].y*cellSize+1, cellSize-2, cellSize-2); 
-    cells[currentFinish[0].y][currentFinish[0].x]=currentFinish[0].type;
+    if (cells[currentFinish.y][currentFinish.x]=="finish"){
+    ctx.fillStyle = currentFinish.type=="wall"?'black':currentFinish.type=="empty"?'white':currentFinish.type=="start"?'green':'red';
+    ctx.fillRect(currentFinish.x*cellSize+1, currentFinish.y*cellSize+1, cellSize-2, cellSize-2); 
+    cells[currentFinish.y][currentFinish.x]=currentFinish.type;
     }
     if (cells[Math.floor(y/cellSize)][Math.floor(x/cellSize)]=="start"){//для отмены стен
       startFinish="finish";
     }
     ctx.fillStyle = 'red';
     ctx.fillRect(Math.floor(x/cellSize)*cellSize+1, Math.floor(y/cellSize)*cellSize+1, cellSize-2, cellSize-2);
-    currentFinish[0]={x:Math.floor(x/cellSize),y:Math.floor(y/cellSize),type:cells[Math.floor(y/cellSize)][Math.floor(x/cellSize)]};
+    currentFinish={x:Math.floor(x/cellSize),y:Math.floor(y/cellSize),type:cells[Math.floor(y/cellSize)][Math.floor(x/cellSize)]};
     cells[Math.floor(y/cellSize)][Math.floor(x/cellSize)]="finish";  
   }
 });
@@ -223,13 +238,13 @@ function aStar()
 }
 function start()
 {
-  if (cells[currentStart[0].y][currentStart[0].x]!="start" && cells[currentFinish[0].y][currentFinish[0].x]!="finish"){
+  if (cells[currentStart.y][currentStart.x]!="start" && cells[currentFinish.y][currentFinish.x]!="finish"){
   alert("please, set start and finish");
   }
-  else if (cells[currentStart[0].y][currentStart[0].x]!="start"){
+  else if (cells[currentStart.y][currentStart.x]!="start"){
     alert("please, set start");
   }
-  else if (cells[currentFinish[0].y][currentFinish[0].x]!="finish"){
+  else if (cells[currentFinish.y][currentFinish.x]!="finish"){
     alert("please, set finish");
   }
 }
